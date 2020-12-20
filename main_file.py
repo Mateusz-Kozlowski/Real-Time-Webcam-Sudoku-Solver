9b81a845c3bf9a0191027ab61bcf61042c0b70be
MateuszKozlowski1124 <70546149+MateuszKozlowski1124@users.noreply.github.com>
2020-12-20 03:11:12 +0100

    Add some comments

--- main_file.py
+++ main_file.py
@@ -1,3 +1,13 @@
+# This is where this program starts and ends.
+# Run this file to check for yourself how the project works.
+# You absolutely don't need to train any digits recognition model by your own.
+# The program uses by default the one I have trained. It is located in Models folder.
+# If you want to change a path to a model
+# just paste the new one as an argument to load_model function at the very top of the main function.
+# The program uses a webcam with id = 0. If you have more than 1 webcam
+# and you want to use different one then you have to change an argument of VideoCapture function to a different number.
+# To exit the program press any key.
+
 print('Importing code and libraries from other files...')
 
 from webcam_sudoku_solver import *
@@ -12,13 +22,14 @@ def main():
 	# TODO dfs all code and ADD interesting print-logs
 	# TODO also add some comments
 	# TODO add "log" to print statements which are logs
+	model = tf.keras.models.load_model('Models/cnn model.h5')
+
 	webcam_width, webcam_height = 1920, 1080
 	webcam = cv.VideoCapture(0)
 	webcam.set(cv.CAP_PROP_FRAME_WIDTH, webcam_width)
 	webcam.set(cv.CAP_PROP_FRAME_HEIGHT, webcam_height)
 
-	model = tf.keras.models.load_model('Models/cnn model.h5', custom_objects=None, compile=True)
-
+	# create the core of the program
 	webcam_sudoku_solver = WebcamSudokuSolver(model)
 
 	while webcam.isOpened():
@@ -27,10 +38,13 @@ def main():
 		if not successful_frame_read:
 			break
 
+		# run the core of the program
 		output_frame = webcam_sudoku_solver.solve(frame)
 
+		# output results
 		cv.imshow('Webcam Sudoku Solver', output_frame)
 
+		# check if a user has pressed a key, if so, close the program
 		if cv.waitKey(1) >= 0:
 			break
 
